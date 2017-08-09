32caba2515ffea3663fd1b9261a018115ca0ae4d
王晨 <voganwong@gmail.com>
2017-08-09 16:33:00 +0800

    1.增加gulp watch css文件的变化

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ var gulp = require('gulp')
 var cssmin = require('gulp-cssmin')
 var rename = require('gulp-rename')
 
-gulp.task('default', function () {
+gulp.task('miniFiles', function () {
   gulp.src('static/css/*.css')
     .pipe(cssmin())
     .pipe(rename({ suffix: '.min'}))
     .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', function () {
+  gulp.watch('static/css/*.css', ['miniFiles'])
 })
