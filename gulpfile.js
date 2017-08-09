0f1c5576d9c293260ea873fd73a810d23119ac75
王晨 <voganwong@gmail.com>
2017-08-09 16:10:29 +0800

    1.add gulpfile 2.add .gitignore 3.add package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,10 @@
 var gulp = require('gulp')
+var cssmin = require('gulp-cssmin')
+var rename = require('gulp-rename')
+
 gulp.task('default', function () {
-  
+  gulp.src('static/css/*.css')
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
 })
