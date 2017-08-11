291ccc9344c71f745fa0ddf7aeb560d08ca92c9e
王晨 <voganwong@gmail.com>
2017-08-11 12:16:00 +0800

    1.增加stylus的编码方式 2.增加浏览器自动监视刷新

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var reload      = browserSync.reload
 var cssmin = require('gulp-cssmin')
 var rename = require('gulp-rename')
+var data = require('gulp-data')
+var stylus = require('gulp-stylus')
 
-gulp.task('miniFiles', function () {
-  gulp.src('static/css/*.css')
-    .pipe(cssmin())
-    .pipe(rename({ suffix: '.min'}))
-    .pipe(gulp.dest('dist'))
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: './'
+  })
+
+  gulp.watch('static/css/*.styl', ['styles'])
+  gulp.watch('index.html').on('change', reload)
 })
 
-gulp.task('watch', function () {
-  gulp.watch('static/css/*.css', ['miniFiles'])
+gulp.task('styles', function () {
+  return gulp.src('static/css/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(reload({stream: true}))
+})
+
+gulp.task('default', function () {
+  gulp.start('browser-sync')
 })
--- readme.md
+++ readme.md
@@ -3,19 +3,23 @@
 > a social Discuss box powered by Hex-Tech
 
 ### Features
----
+
 - use it easily
-- show contents friendly
+- show contents friendly in PC or Mobile
 - [ ] support Markdown text
 
+### Technology Stack
+- Gulp
+- Stylus
+
 ### Usage
----
+
 \# TODO:
 
 ### Demo
----
+
 [https://discuss.hex-tech.io](https://discuss.hex-tech.io)
 
 ### Contributions
----
+
 We hope you will support our projects and report issues. :)
